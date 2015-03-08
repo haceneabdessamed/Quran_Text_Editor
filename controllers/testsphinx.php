e9afeb7f7ce421ed9027196ee9e332a9b960057f
haceneabdessamed <aa8hacene@gmail.com>
2015-03-08 22:43:59 +0100

    add sphinxtest

---
+++ controllers/testsphinx.php
@@ -0,0 +1,31 @@
+<?php
+require 'C:\sphinx\api\sphinxapi.php';
+header('Content-Type: text/html; charset=utf-8');
+echo('الصمد');
+$cl = new SphinxClient();
+$cl->SetServer('127.0.0.1', 9300);
+$cl->SetLimits(0,1000000);
+
+$cl->AddQuery($_POST["query"], 'test1');
+$result = $cl->RunQueries();
+echo '<pre>';
+
+  if ($result == false)
+  {
+   echo 'Query failed: ' . $cl->GetLastError() . "\n";
+  }
+   
+   if ($cl->GetLastWarning())
+   {
+    echo 'WARNING: ' . $sphinx->GetLastWarning() . "\n";
+   }
+   foreach($result[0]['matches'] as $x => $x_value) {
+    echo "id=" . $x;
+    echo "<br>";
+}
+   print_r($result);
+
+$vowels = array("َ","ً","ُ","ٌ","ٌ","ِ","ٍ","ْ","ّ","ٰ");
+$onlyconsonants = str_replace($vowels, "", "إِيَّاكَ نَعْبُدُ وَإِيَّاكَ نَسْتَعِينُ");
+echo $onlyconsonants;
+?>	
