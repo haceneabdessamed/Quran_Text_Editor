f2e51f7320328b7197b3bec1f7b946697a7ea40f
haceneabdessamed <aa8hacene@gmail.com>
2015-03-16 22:54:49 +0100

    r

--- controllers/readverset.php
+++ controllers/readverset.php
@@ -1,6 +1,7 @@
 <?php
 
-
+require '../classes/aya.php';
+require '../classes/SqlConnexion.php';
 function getVetset($soura,$aya){
 	$filename = "quran-simple.xml";
 	$xml_file=simplexml_load_file($filename);
@@ -10,13 +11,21 @@ function getVetset($soura,$aya){
 
 function getSoura($id)
 {
+	$connection =new SqlConexion("localhost","root","","quran");
+	$connection->connecter();
+	echo "<pre>";
+	$stmt=$connection->SelectQuery("select * from quran where ID=".$id);
+	$row = $stmt->fetch();
+	$aya=new aya(0,0,0);
+    $aya->id=$id;
+	$aya->souraId=$row['SuraID'];
+    $aya->ayaId=$row['VerseID'];
+	echo getVetset($aya->souraId, $aya->ayaId);
+	return $aya;
 	
 }
 
-function getAya($soura)
-{
-	
-}
+
 ?>
 
 
